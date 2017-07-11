189b89d68ad7e64857b7f6ea4a97fed69d7b4560
727175929 <727175929@qq.com>
2017-07-11 13:35:12 +0800

    实现了地图定位

---
+++ MyApplication/app/src/main/java/zx31401425/zucc/mycurrencies/wxapi/WXEntryActivity.java
@@ -0,0 +1,15 @@
+package zx31401425.zucc.mycurrencies.wxapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zx31401425.zucc.mycurrencies.R;
+
+public class WXEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wxentry);
+    }
+}
